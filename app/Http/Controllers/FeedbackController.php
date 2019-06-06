c566a220d3c9e2c1cc23be7f0a05d411ecb3798e
Ari Jati Permana <arijatipermana@gmail.com>
2019-06-06 22:32:37 +0000

    Menambahkan function pada FeedbackController

--- app/Http/Controllers/FeedbackController.php
+++ app/Http/Controllers/FeedbackController.php
@@ -2,9 +2,77 @@
 
 namespace App\Http\Controllers;
 
+use App\Events\FeedbackReceived;
+use App\Feedback;
 use Illuminate\Http\Request;
 
 class FeedbackController extends Controller
 {
-    //
+    public function store(Request $request)
+    {
+        $words = $request->get('words');
+
+        // tidak boleh mengandung koma (,).
+        if (strpos($words, ",") !== false) {
+            return response()->json([
+                "message" => "Kata tidak boleh mengandung tanda koma (,).",
+            ], 400);
+        }
+
+        $words = explode(" ", $words);
+
+        // tidak boleh lebih dari 3 kata.
+
+        if (count($words) > 3) {
+            return response()->json([
+                "message" => "Tidak boleh melebihi dari 3 kata.",
+            ], 400);
+        }
+
+        foreach ($words as $key => $word) {
+            $this->createOrIncrement($word);
+        }
+
+        return response()->json("OK");
+    }
+
+    public function dashboard()
+    {
+        return view('dashboard');
+    }
+
+    public function dashboardData()
+    {
+        return response()->json($this->getData());
+    }
+
+    protected function getData()
+    {
+        $top_ten = Feedback::orderBy('count', 'DESC')
+            ->get()
+            ->take(10);
+        return $top_ten;
+    }
+
+    public function input()
+    {
+        return view('input');
+    }
+
+    protected function createOrIncrement(String $word)
+    {
+        // jadikan lowercase.
+        $word = strtolower($word);
+
+        $feedback = Feedback::where('word', $word)->first();
+
+        if ($feedback) {
+            $feedback->increment('count');
+        } else {
+            Feedback::create([
+                "word"  => $word,
+                "count" => 1,
+            ]);
+        }
+    }
 }
